68ac22d0e18061cccc26dbfd1142a64458299deb
Panda-Abhisek <abhisekpanda114@gmail.com>
2026-03-10 22:19:59 +0530

    feat(main): wrap app with theme and tooltip providers
    - Add ThemeProvider for dark/light mode support
    - Add TooltipProvider for tooltip components
    - Prepare app for new sidebar architecture

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthProvider'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './components/theme-provider'
+import { TooltipProvider } from './components/ui/tooltip'
+import { SidebarProvider } from './components/ui/sidebar'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <ThemeProvider>
+        <TooltipProvider>
+          {/* <SidebarProvider> */}
+            <App />
+          {/* </SidebarProvider> */}
+        </TooltipProvider>
+      </ThemeProvider>
     </AuthProvider>
   </BrowserRouter>
 )
